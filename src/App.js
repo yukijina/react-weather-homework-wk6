3c49664454a00e2e3868780004146403a4d7e76e
yuki <38411009+yukijina@users.noreply.github.com>
2025-07-18 06:35:00 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import Form from './Form';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-heading-1'>Weather App</h1>
-      </header>
-      <Form defaultCity='New York' />
+      <div className='App-container'>
+        <header className='App-header'>
+          <h1 className='App-heading-1'>Weather App</h1>
+        </header>
+        <Form defaultCity='New York' />
+      </div>
+      <footer className='App-footer'>
+        <p>
+          This project was coded by Yukiji and is open-sourced on{' '}
+          <a
+            href='https://github.com/yukijina/react-weather-homework-wk6'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            GitHub
+          </a>{' '}
+          and hosted on{' '}
+          <a href='#' target='_blank' rel='noopener noreferrer'>
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
